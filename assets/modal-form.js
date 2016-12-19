5558a3b01159ea0196e8026e12283feaa73a3a5b
Andrey Borodulin <borodulin@gmail.com>
2016-12-20 01:18:54 +0500

    improve js

--- assets/modal-form.js
+++ assets/modal-form.js
@@ -12,24 +12,24 @@
         var self = this;
         var idCounter = 0;
         this.defaultOptions = {
-            size : "",
-            loginUrl : "",
-            loadingContent : '<div class="modal-header">Loading...</div>'
+            size : null,
+            loginUrl : null,
+            loading : '<div class="modal-header">Loading...</div>'
                     + '<div class="modal-body">'
                     + '<div class="progress progress-striped active" style="margin-bottom:0;">'
                     + '<div class="progress-bar" style="width: 100%"></div>'
                     + '</div>' + '</div>',
-            modalDialog : '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">'
+            modal : '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">'
                     + '<div class="modal-dialog">'
-                    + '<div class="modal-content"></div>' + '</div>' + '</div>'
+                    + '<div class="modal-content"></div>' + '</div>' + '</div>',
+            error : '<div class="error-summary"></div>'
         };
 
         var ajaxContent = function(ajax, $modalDialog) {
-            var $content = $modalDialog.loadingContent();
-            $modalDialog.modal('show');
+            $modalDialog.modal('show').loading();
             $.ajax($.extend(ajax, {
                 success : function(response) {
-                    $content.html(response);
+                    $modalDialog.content(response);
                 },
                 error : function(jqXHR) {
                     if ((jqXHR.status === 401) && (settings.loginUrl)) {
@@ -37,20 +37,25 @@
                             url : settings.loginUrl
                         });
                     } else {
-                        $content.html('<div class="error-summary">'
-                                + jqXHR.responseText + '</div>');
+                        $modalDialog.error(jqXHR.responseText);
                     }
                 }
             }));
         };
-        var doModal = function(options) {
+        var createModalDialog = function(options) {
             var settings = $.extend({}, self.defaultOptions, options);
-            var $modalDialog = $(settings.modalDialog).attr('id',
+            var $modalDialog = $(settings.modal).attr('id',
                     'modalFormId-' + ++idCounter);
             $('body').append($modalDialog);
-            $modalDialog.loadingContent = function() {
-                return $modalDialog.find('.modal-content').html(
-                        settings.loadingContent);
+            var $content = $modalDialog.find('.modal-content');
+            $modalDialog.loading = function() {
+                return $content.html(settings.loading);
+            };
+            $modalDialog.error = function($error) {
+                return $content.html($(settings.error).html($error));
+            };
+            $modalDialog.content = function(html) {
+                return $content.html(html);
             };
             $modalDialog.on('hidden.bs.modal', function(e) {
                 $(this).remove();
@@ -67,10 +72,8 @@
                     ajax.data = new FormData(form);
                     ajax.type = 'post';
                     ajax.enctype = 'multipart/form-data';
-                    ajax.processData = false; // tell jQuery not to process
-                                                // the data
-                    ajax.contentType = false; // tell jQuery not to set
-                                                // contentType
+                    ajax.processData = false; // tell jQuery not to process the data
+                    ajax.contentType = false; // tell jQuery not to set contentType
                 } else {
                     ajax.data = $form.serialize();
                 }
@@ -83,12 +86,10 @@
         return this.each(function() {
             var $this = $(this);
             $this.on('click', function() {
-                var $self = $(this), url = $self.data('url')
-                        || $self.attr('href');
-                $modalDialog = doModal($this.data());
-                ajaxContent({
-                    url : url
-                }, $modalDialog);
+                var $self = $(this);
+                var url = $self.data('url') || $self.attr('href');
+                $modalDialog = createModalDialog($this.data());
+                ajaxContent({url : url}, $modalDialog);
                 return false;
             });
         });
